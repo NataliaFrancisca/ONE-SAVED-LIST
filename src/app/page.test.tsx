36fc3ff7033a63f586f2d7e763b4ce3bb5f5b62e
Natalia Francisca <nathifrancisca@gmail.com>
2024-04-05 21:47:17 -0300

    feat: created a test to the HomePage component

---
+++ src/app/page.test.tsx
@@ -0,0 +1,40 @@
+import { expect, test, describe } from 'vitest'
+import { render, screen } from "@testing-library/react";
+import { userEvent} from "@testing-library/user-event";
+import React from 'react';
+import Home from './page';
+import mockRouter from 'next-router-mock';
+
+describe('HomePage component', () => {
+  test('Should render the title', () => {
+    render(<Home />);
+    const title = screen.getByText('ONE. SAVED LIST');
+    expect(title).toBeDefined();
+  })
+
+  test('Should render the illustration', () => {
+    render(<Home />);
+    const illustration = screen.getByRole('img');
+    expect(illustration).toBeDefined();
+  });
+
+  test('Should render the button [GET STARTED]', () => {
+    render(<Home />);
+    const buttonNavigation = screen.getByRole('button');
+    expect(buttonNavigation).toBeDefined();
+  }); 
+
+  test('Should the button be clicked and navigate to [Register] page', async() => {
+    render(<Home />);
+
+    const button = screen.getByRole('button');
+    await userEvent.click(button);
+
+    expect(mockRouter).toMatchObject({ 
+      asPath: "/register",
+      pathname: "/register",
+      query: {},
+    });
+  });
+})
+
