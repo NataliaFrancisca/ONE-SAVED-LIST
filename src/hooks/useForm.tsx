f98d1b562892dce3922a86b646b77f48d0fc2623
Natalia Francisca <nathifrancisca@gmail.com>
2024-04-08 19:06:49 -0300

    feat: created a custom hook to use on Login and Register page

---
+++ src/hooks/useForm.tsx
@@ -0,0 +1,33 @@
+import { setCookie } from "@/service/cookies";
+import { FormAuthService } from "@/service/form";
+import { type IForm, type IErrorForm, type IFormRegister, type IFormAuthType} from "@/ts/interface";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export const useForm = (response: IForm, type: IFormAuthType) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<IErrorForm>();
+
+  const router = useRouter();
+
+  const onSubmit = async(submitWithGoogle = false) => {
+    event?.preventDefault();
+
+    setLoading(true);
+
+    const responseService = type.LOGIN 
+      ? await FormAuthService(response, submitWithGoogle, {LOGIN: true}) 
+      : await FormAuthService(response as IFormRegister, submitWithGoogle, {REGISTER: true});
+    
+    setError(responseService.error);
+
+    if(responseService.status && responseService.data){
+      setCookie(responseService.data);
+      router.push('/dashboard');
+    }
+
+    setLoading(false);
+  }
+
+  return { error, loading, onSubmit }
+}
