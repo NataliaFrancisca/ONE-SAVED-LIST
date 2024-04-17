b5392654d368262daef19ad08c60ad88f6f5b0c4
Natalia Francisca <nathifrancisca@gmail.com>
2024-04-17 15:02:09 -0300

    feat: added a data-test to the loader

--- src/app/components/Loader/Loader.tsx
+++ src/app/components/Loader/Loader.tsx
@@ -1,7 +1,7 @@
 import style from "./Loader.module.css";
 
 const Loader = (props: { color: string }) => {
-  return <span className={`${style.loader} ${style[`_${props.color}`]}`}></span>;
+  return <span className={`${style.loader} ${style[`_${props.color}`]}`} data-test='loader'></span>;
 };
   
 export default Loader;
