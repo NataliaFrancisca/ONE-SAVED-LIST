8bff4c189bc095f89b159440bb6c9b9075ee1fc6
nataliaFrancisca <nathifrancisca@gmail.com>
2024-05-11 22:23:50 -0300

    feat: created a custom hook to handle with the input search logic

---
+++ src/app/components/Dashboard/Input/useInputSearch.tsx
@@ -0,0 +1,28 @@
+import { useInputValue, useToggleInput } from "@/hooks/useRedux";
+import { useRouter } from 'next/navigation'
+import { useState } from "react";
+
+export const useInputSearch = () => {
+  const [inputSearchValue, setInputSearchValue] = useState<undefined | string>(undefined);
+  
+  const { onToggleInput } = useToggleInput();
+  const { onUpdateInputValue } = useInputValue()
+
+  const router = useRouter();
+
+  const onSubmit = () => {
+    event?.preventDefault();
+
+    if(inputSearchValue){
+      onUpdateInputValue(inputSearchValue.toUpperCase())
+      router.push('/dashboard');
+    }
+  }
+
+  const onCloseInputSearch = () => {
+    event?.preventDefault();
+    onToggleInput();
+  }
+
+  return { inputSearchValue, setInputSearchValue, onSubmit, onCloseInputSearch }
+}
